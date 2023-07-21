75a4338b956afd581936ab47d19976edbe0218a4
zubbycharles <zubbycharles29@gmail.com>
2023-07-21 16:19:41 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - tests the postitive_or_negative() function at zero
+ * Return: 0
+ */
+int main(void)
+{
+        int i;
+        i = 0;
+        positive_or_negative(i);
+        return (0);
+}
